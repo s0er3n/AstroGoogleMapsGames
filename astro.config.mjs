cc485cd9a9100658ab8d80fbf2454f88cc1c8626
Sören Michaels <soeren.michaels@gmail.com>
2022-07-27 21:30:10 +0200

    optimized images

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import { astroImageTools } from "astro-imagetools";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  experimental: { integrations: true },
+  integrations: [tailwind(), astroImageTools],
+})
